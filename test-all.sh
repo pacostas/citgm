c9c6188c5f704d64680c1bf84258cb12e7a85866
Costas Papastathis <papastathiscr@gmail.com>
2023-05-17 17:32:57 +0300

    adding jq back

--- test-all.sh
+++ test-all.sh
@@ -2,16 +2,18 @@
 
 json_file="./lib/lookup.json"
 
-# Extract attribute names using grep and sed
-attribute_names=$(grep -o '^\s*"[^"]*"' "$json_file" | sed 's/"//g')
+# Extract attribute names using jq from the file
+attribute_names=$(jq -r 'keys[]' "$json_file")
 
 for attribute_name in $attribute_names; do
     echo "$attribute_name"
     if npx citgm "$attribute_name"; then
         echo "$attribute_name command succeeded."
+        echo "$attribute_name" >>citgm_passed.txt # Append "$attribute_name" to output.txt
+
     else
         echo "$attribute_name command failed."
-        echo "$attribute_name" >>output.txt # Append "$attribute_name" to output.txt
+        echo "$attribute_name" >>citgm_failed.txt # Append "$attribute_name" to output.txt
     fi
 done
 
